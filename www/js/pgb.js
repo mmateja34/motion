9c419fbc5512942a75db22f38fafd77b910818e7
mmateja <mmateja34@gmail.com>
2017-03-23 11:52:07 +0100

    added connections plugin and functions

---
+++ www/js/pgb.js
@@ -0,0 +1,28 @@
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
+}
+
+function isConnected() {
+    document.addEventListener("offline", onOffline, false);
+    document.addEventListener("online", onOnline, false);
+}
+
+function onOnline() {
+    navigator.notification.alert("Obecnie masz połączenie z internetem");
+}
+
+function onOffline() {
+    avigator.notification.alert("Obecnie nie masz połączenia z internetem");
+}
